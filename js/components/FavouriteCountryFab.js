e0b74b4e619c1d9d79d73012fcd092a2f8ad06c9
Constantin Catalina <catalina.constantin@cognizant.com>
2026-01-16 12:04:15 +0200

    fix: fab ui

--- js/components/FavouriteCountryFab.js
+++ js/components/FavouriteCountryFab.js
@@ -2,7 +2,7 @@ export function createFab({ onClick } = {}) {
     const fab = document.createElement('button');
     fab.className = 'fab';
     fab.innerHTML = `
-        <svg viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false">
+        <svg class="star-svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false">
             <polygon points="12,2 15,9 22,9.5 17,14.5 18.5,22 12,18 5.5,22 7,14.5 2,9.5 9,9" />
         </svg>
     `;
